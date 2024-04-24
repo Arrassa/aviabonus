1e67f23ab33a9e3ff4382dd7ccd6e84b5bc03db3
aleksandr.b <ser.sash@gmail.com>
2024-04-24 19:54:01 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 20000;
+        int bonusmiles = (int) (ticketprice / 20);
+        System.out.println("начисленные бонусы:" + bonusmiles);
+
+
+    }
+}
